f88b9c9195da1195f168b9c2fb65481c3b1cf3bd
rodon557 <546492510@qq.com>
2019-03-01 14:27:51 +0800

    add some commond

--- src/test/java/cn/pompip/TestSome.java
+++ src/test/java/cn/pompip/TestSome.java
@@ -6,7 +6,10 @@ import cn.pompip.adb.App;
 import cn.pompip.adb.Tools;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,7 +23,12 @@ public class TestSome {
             AndroidDevice androidDevice = new AndroidDevice(device);
             App app = new App();
 //            androidDevice.installApp(app);
-            androidDevice.findLauncherActivity(app);
+//            androidDevice.findLauncherActivity(app);
+
+//            androidDevice.root();
+//            androidDevice.exeCommand("su -c ' /data/data/com.bonree.mobile.base/files/busybox unzip -o /data/local/tmp/bonree.zip -d /data/local/tmp/bonree/'");
+//            androidDevice.exeCommand("su & mount -o rw,remount /system & touch /system/hello4");
+            androidDevice.exeCommand("su -c chmod 777 /data/local/tmp/utils/busybox");
         });
         tools.start(taskArrayList);
     }
@@ -36,6 +44,34 @@ public class TestSome {
             Log.i(this,group);
         }
 
+    }
+
+    @Test
+    public void testOdd(){
+        System.out.println(0&1);
+        System.out.println(0|1);
+        System.out.println(0^1);
 
     }
+
+    @Test public void testObjArray(){
+        TestObj[] testObjs = {new TestObj(1),new TestObj(2),new TestObj(3)};
+        TestSum testSum = new TestSum();
+        testSum.testObjs = testObjs;
+
+        try {
+//            Class<?> cArray = Class.forName("[Lcn.pompip.TestObj");
+            Class<?> testSumClass = Class.forName("cn.pompip.TestSum");
+            Field field  = testSumClass.getField("testObjs");
+
+            Object o = field.get(testSum);
+
+            Log.i(this,o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
